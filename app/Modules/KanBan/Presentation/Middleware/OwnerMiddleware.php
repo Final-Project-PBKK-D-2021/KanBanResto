171f5810bfd5f549bffee8a808c72def65d7776d
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2021-05-28 00:14:59 +0700

    Temporary Implemented Middleware

---
+++ app/Modules/KanBan/Presentation/Middleware/OwnerMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Modules\KanBan\Presentation\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OwnerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+        if (!Auth::guard('owner')->check()) {
+            return redirect('/');
+        }
+    }
+}
